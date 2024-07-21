9b0e09b75198d58a1879fab53acc6696e64d7c1a
arpansahu <arpanrocks95@gmail.com>
2024-07-21 14:48:04 +0530

    corrected shell list

--- update_all_projects_readme_htmls.sh
+++ update_all_projects_readme_htmls.sh
@@ -21,7 +21,7 @@ EOF
 
 # List of project repositories to clone
 REPOS=(
-    "https://github.com/arpansahu/great_chat",
+    "https://github.com/arpansahu/great_chat"
     "https://github.com/arpansahu/arpansahu_dot_me"
     # Add more repositories as needed
 )
