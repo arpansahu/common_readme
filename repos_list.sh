eb955b833f50fe4b5c5bafa25647777f4a590033
arpansahu <arpanrocks95@gmail.com>
2024-08-05 19:37:20 +0530

    updated repo list for chew and cheers

--- repos_list.sh
+++ repos_list.sh
@@ -3,5 +3,6 @@
 REPOS=(
     "https://github.com/arpansahu/great_chat"
     "https://github.com/arpansahu/arpansahu_dot_me"
+    "https://github.com/arpansahu/chew_and_cheer"
     # Add more repositories as needed
 )
