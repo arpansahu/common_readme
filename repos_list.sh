ed0707501f8dda754fa5c5193b3a48c150cd1aee
arpansahu <arpanrocks95@gmail.com>
2024-08-07 01:10:23 +0530

    added numericals in repo list

--- repos_list.sh
+++ repos_list.sh
@@ -4,5 +4,6 @@ REPOS=(
     "https://github.com/arpansahu/great_chat"
     "https://github.com/arpansahu/arpansahu_dot_me"
     "https://github.com/arpansahu/chew_and_cheer"
+    "https://github.com/arpansahu/numerical"
     # Add more repositories as needed
 )
