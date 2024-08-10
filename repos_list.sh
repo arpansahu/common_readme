541d65ecec362c016b1f92836c9ea85402141bc9
arpansahu <arpanrocks95@gmail.com>
2024-08-10 11:48:12 +0530

    updated repo list

--- repos_list.sh
+++ repos_list.sh
@@ -1,9 +1,11 @@
 # repos_list.sh
 
 REPOS=(
-    "https://github.com/arpansahu/great_chat"
     "https://github.com/arpansahu/arpansahu_dot_me"
     "https://github.com/arpansahu/chew_and_cheer"
+    "https://github.com/arpansahu/clock_work"
+    "https://github.com/arpansahu/django_starter"
+    "https://github.com/arpansahu/great_chat"
     "https://github.com/arpansahu/numerical"
     "https://github.com/arpansahu/school_chale_hum"
     # Add more repositories as needed
