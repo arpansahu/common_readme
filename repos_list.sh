566b3468c05dbdbb10a9baa08477fb4d151aa885
Arpan Sahu <arpanrocks95@gmail.com>
2024-08-30 13:51:58 +0530

    altered datum api added to repo lists

--- repos_list.sh
+++ repos_list.sh
@@ -10,5 +10,6 @@ REPOS=(
     "https://github.com/arpansahu/numerical"
     "https://github.com/arpansahu/school_chale_hum"
     "https://github.com/arpansahu/third_eye"
+    "https://github.com/arpansahu/altered_datum_api"
     # Add more repositories as needed
 )
