2cdf4178810d18e0c7a9f156ff9382468e31338f
Arpan Sahu <arpanrocks95@gmail.com>
2024-11-04 16:55:27 +0530

    updated repo list

--- repos_list.sh
+++ repos_list.sh
@@ -10,6 +10,7 @@ REPOS=(
     "https://github.com/arpansahu/numerical"
     "https://github.com/arpansahu/school_chale_hum"
     "https://github.com/arpansahu/third_eye"
-    "https://github.com/arpansahu/altered_datum_api"
+    "https://github.com/arpansahu/altered_datum_api",
+    "https://github.com/arpansahu/scrape_optimus"
     # Add more repositories as needed
 )
